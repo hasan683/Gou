85da14446fae2fe5e37a60adf6417d96c4deba07
hasan683 <hsourov511@gmail.com>
2021-02-20 18:30:43 +0600

    modification

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <body>
 
 
-   <!-- <section class="header">
+    <!-- <section class="header">
         <nav id="menu-part" class="navbar navbar-expand-lg p-0  main-menu shadow-sm">
             <div class="container-lg ps-3">
                 <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
@@ -58,35 +58,41 @@
         </nav>
     </section>-->
     <nav class="navbar navbar-expand-lg main-menu navbar-light shadow-sm">
-  <div class="container">
-  <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="index-page.php">Home</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="online-course.php">Courses</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="online-exam.php">Exams</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="about-page.php">About</a>
-        </li>
-      </ul>
-      <div class="hot-line">
-    <i class="icon fad fa-phone-plus pe-3"></i>
-    <span class="number">Hot-Line:01404432536</span>
-    </div>
-      <form class="d-flex box-1">
-     
-      <a href="profile.php" button type="button" class="btn profile-btn">Profile</a>
-      <a href="login.php" button type="button" class="btn btn-outline-warning sign-out-btn">Sign Out</a>
-      </form>
-    </div>
-  </div>
-</nav>
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index-page.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="online-course.php">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="online-exam.php">Exams</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about-page.php">About</a>
+                    </li>
+                    <li class="hot-line">
+                        <i class="icon fad fa-phone-plus pe-3"></i>
+                        <span class="number">Hot-Line:01404432536</span>
+                    </li>
+                    <li class="box-1">
+                        <a href="profile.php" button type="button" class="btn profile-btn">Profile</a>
+                        <a href="login.php" button type="button" class="btn btn-outline-warning sign-out-btn">Sign
+                            Out</a>
+                    </li>
+                </ul>
+
+
+
+
+            </div>
+        </div>
+    </nav>
--- online-exam.php
+++ online-exam.php
@@ -2,65 +2,65 @@
 <section class="bg-white card-menu">
     <div class="container mt-3">
     <div class="row">
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:1</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                        <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                        <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:2</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                            <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                            <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:3</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                            <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                            <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:1</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                            <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                            <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:2</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                            <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                            <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 px-4">
+            <div class="col-sm-4">
                 <div class="shadow card text-center mt-4">
                     <div class="card-body">
                         <h5 class="card-title text-primary">Online Exam Name:3</h5>
                         <p class="card-text text-primary">Duration:1 Hour<br>
                             Course Fee:50 tk</p>
-                            <a href="#" type="button" class="btn btn-warning px-5 text-white mb-3">Purchase Now</a>
+                            <a href="#" type="button" class="btn btn-warning px-5 text-white">Purchase Now</a>
                     </div>
                 </div>
             </div>
